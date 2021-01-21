b60fa93a03c1172e63a4e79bb62a4eb28597852e
buitrunghieu <buitrunghieu243@github.com>
2021-01-21 04:30:19 +0000

    2021-01-21

--- pages/detail.php
+++ pages/detail.php
@@ -1,38 +1,81 @@
 <?php
-    $dsn = "mysql:host=localhost;dbname=restaurantdb;charset=utf8";
-    $user = "restaurantdb_admin";
-    $password = "admin123";
-    
-     isset($_GET["id"]) ? $id = $_GET["id"] : $id = "";
-    
-    try {
-    	$pdo = new PDO($dsn, $user, $password);
-        // 実行するSQLを設定
-        $sql = "select * from restaurants where id = ?";
-        $sqlRv = "select * from reviews where restaurant_id = ?";
-        // SQL実行オブジェクトを取得
-        $pstmt = $pdo->prepare($sql);
-        $pstmtRv = $pdo->prepare($sqlRv);
-        // プレースホルダにリクエストパラメータを設定
-        $pstmt->bindValue(1, $id);
-        $pstmtRv->bindValue(1, $id);
-        // SQLを実行
-        $pstmt->execute();
-        $pstmtRv->execute();
-        // SQL実行結果を配列に取得
-        $records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
-        $recordsRv = $pstmtRv->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) { 
-    	echo $e->getMessage();
-    } 
-
-    //*****
-    var_dump($id);
-    var_dump($records);
-    var_dump($recordsRv);
+/**
+ * 評価ポイントを星印文字列に変換する（作業-5）。
+ * @param int 評価ポイント
+ * @return string 星印文字列
+ */
+function convertPointsToStars($point):string {
+	$stars = "";
+	for ($i = 0; $i < 5; $i++) {
+		if ($i < $point) {
+			$stars .= "★";
+		} else {
+			$stars .= "☆";
+		}
+	}
+	return $stars;
+}
 ?>
-<!DOCTYPE html>
-<html lang="ja">
+<?php
+/** リクエストパラメータを取得 */
+isset($_GET["id"]) ? $id = $_GET["id"] : $id = 0;
+// var_dump($id);	// 【作業-1】
+
+/** データベース接続情報を設定 */
+$dsn = "mysql:host=localhost;dbname=restaurantdb;charset=utf8";
+$user = "restaurantdb_admin";
+$password = "admin123";
+
+// データベース接続オブジェクトを取得
+$pdo = new PDO($dsn, $user, $password);
+
+/** レストランを取得 */
+// レストランを取得するQLを設定
+$sql = "select * from restaurants where id = ?";
+// SQL実行オブジェクトを取得
+$pstmt = $pdo->prepare($sql);
+// プレースホルダにリクエストパラメータを設定
+$pstmt->bindValue(1, $id);
+// SQLを実行
+$pstmt->execute();
+// SQL実行結果を配列に取得
+$records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($records);	// 【作業-2】
+
+// レストランを取得
+$resutaurant = null;
+if (count($records) > 0) {
+	$restaurant = $records[0];
+}
+
+/** レストランに対するレビュを取得 */
+// レビュを取得するQLを設定
+$sql = "select * from reviews where restaurant_id = ?";
+// SQL実行オブジェクトを取得
+$pstmt = $pdo->prepare($sql);
+// プレースホルダにリクエストパラメータを設定
+$pstmt->bindValue(1, $id);
+// SQLを実行
+$pstmt->execute();
+// SQL実行結果を配列に取得
+$records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($records);	// 【作業-3】
+
+// レビュを取得
+$reviews = [];
+foreach ($records as $record) {
+	$review = [];
+	$review["id"] = $record["id"];
+	$review["restaurant_id"] = $id;
+	$review["reviewer"] = $record["reviewer"];
+	$review["comment"] = $record["comment"];
+	$review["point"] = $record["point"];
+	$review["posted_at"] = $record["posted_at"];
+	$reviews[] = $review;
+}
+?>
+<!DOCTYPE php>
+<php lang="ja">
 <head>
 	<meta charset="UTF-8" />
 	<title>レストランレビュサイト - 小テスト</title>
@@ -47,44 +90,38 @@
 	<main>
 		<article class="detail">
 			<h2>レストラン詳細</h2>
+			<?php if (!is_null($restaurant)): ?>
 			<section>
 				<table class="list">
-				    <?php foreach ($records as $record): ?>
-    					<tr>
-    						<td class="photo"><img name="image" src="../pages/img/<?= $record["image"] ?>" /></td>
-    						<td class="info">
-    							<dl>
-    								<dt name="name"><?= $record["name"] ?></dt>
-    								<dd name="description">
-    									<?= $record["description"] ?>。
-    								</dd>
-    							</dl>
-    						</td>
-    					</tr>
-					<?php endforeach; ?>
+					<tr>
+						<td class="photo"><img name="image" src="../pages/img/<?= $restaurant["image"] ?>" /></td>
+						<td class="info">
+							<dl>
+								<dt name="name"><?= $restaurant["name"] ?></dt>
+								<dd name="description"><?= $restaurant["description"] ?></dd>
+							</dl>
+						</td>
+					</tr>
 				</table>
 			</section>
+			<?php endif; ?>
 		</article>
 		<article class="reviews">
 			<h2>レビュ一覧</h2>
-			<?php if (count($recordsRv) > 0): ?>
+			<?php if (count($records) > 0): ?>
 			<section>
-			    <?php foreach ($recordsRv as $recordRv): ?>
-    				<dl class="review">
-    					<dt name="point" id="point">
-    					    <?php
-					        $point = intval($recordRv["point"]);
-					        $str = str_repeat("★", $point).str_repeat("☆", 5 - $point);
-					        echo $str;
-                            ?>
-    					</dt>
-    					<dd name="description">
-    							<?= $recordRv["comment"] ?>
-    							<div name="posted">
-    								（<span name="posted_at"><?= $recordRv["posted_at"] ?></span><span name="reviewer"><?= $recordRv["reviewer"] ?></span>さん）
-    							</div>
-    					</dd>
-    				</dl>
+				<?php foreach ($reviews as $review): ?>
+				<dl class="review">
+					<!--【作業-4】画面ショットが取れたらコメントアウト
+					<dt name="point"><?= $review["point"] ?></dt>
+					-->
+					<dt name="point"><?= convertPointsToStars($review["point"]) ?></dt>	<!-- 【作業-5】 -->
+					<dd name="description"><?= $review["comment"] ?>
+							<div name="posted">
+								（<span name="posted_at"><?= $review["posted_at"] ?></span><span name="reviewer"><?= $review["reviewer"] ?></span>さん）
+							</div>
+					</dd>
+				</dl>
 				<?php endforeach; ?>
 			</section>
 			<?php endif; ?>
@@ -120,10 +157,10 @@
 					<div class="buttons">
 						<input type="submit" value="投稿" />
 						<input type="reset" value="クリア" />
+						<input type="hidden" name="id" value="<?= $restaurant["id"] ?>" />
 					</div>
 				</form>
 			</section>
-			
 		</article>
 	</main>
 	<footer>
@@ -131,4 +168,4 @@
 	</footer>
 	</div>
 </body>
-</html>
+</php>
--- pages/list.php
+++ pages/list.php
@@ -1,37 +1,29 @@
 <?php
-    $dsn = "mysql:host=localhost;dbname=restaurantdb;charset=utf8";
-    $user = "restaurantdb_admin";
-    $password = "admin123";
-    
-     isset($_GET["area"]) ? $area = $_GET["area"] : $area = "";
-    
-    try {
-    	$pdo = new PDO($dsn, $user, $password);
-    	$sql = "select * from restaurants where area=?";
-    	$pstmt = $pdo->prepare($sql);
-    	$pstmt->bindValue(1, $area);
-    	$pstmt->execute();
-    	$records = [];
-    	$records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
-    	$stmt = $pdo->prepare("select * from restaurants");
-    	$stmt->execute();
-    	$allData = [];
-    	$allData = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    	unset($pstmt);
-    	unset($stmt);
-    	unset($pdo);
-    } catch (PDOException $e) { 
-    	echo $e->getMessage();
-    } 
+/** リクエストパラメータを取得 */
+isset($_POST["area"]) ? $area = $_POST["area"] : $area = "";
+//var_dump($area);	// 作業-4
+/** データベース接続情報を設定 */
+$dsn = "mysql:host=localhost;dbname=restaurantdb;charset=utf8";
+$user = "restaurantdb_admin";
+$password = "admin123";
 
-
-   
-    //$data = var_dump($area);
-    //$data2 = var_dump($records);
+// データベース接続オブジェクトを取得
+$pdo = new PDO($dsn, $user, $password);
+// 実行するSQLを設定
+$sql = "select * from restaurants where area = ?";
+// SQL実行オブジェクトを取得
+$pstmt = $pdo->prepare($sql);
+// プレースホルダにリクエストパラメータを設定
+$pstmt->bindValue(1, $area);
+// SQLを実行
+$pstmt->execute();
+// SQL実行結果を配列に取得
+$records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($records); // 作業-5
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
+<!DOCTYPE php>
+<php lang="ja">
 <head>
 	<meta charset="UTF-8" />
 	<title>レストランレビュサイト - 小テスト</title>
@@ -47,7 +39,7 @@
 			<div class="clearfix">
 			<h2>レストラン一覧</h2>
 			<section class="entry">
-				<form action="list.php" method="get">
+				<form action="list.php" method="post">
 					<select name="area">
 						<option value="">-- 地域を選んでください --</option>
 						<option value="福岡">福岡</option>
@@ -58,46 +50,29 @@
 				</form>
 			</section>
 			</div>
+			<?php if (count($records) > 0): ?>
 			<section class="result">
-			    <?php if(count($records) == 0): ?>
-			        <p><?= count($allData) ?>件のレストランが見つかりました。</p>
-    				<table class="list">
-    				    <?php foreach ($allData as $data): ?>
-        					<tr>
-        						<td class="photo"><img name="image" alt="「Wine Bar ENOTECA」の写真" src="../pages/img/<?= $data[image]?>" /></td>
-        						<td class="info">
-        							<dl>
-        								<dt name="name"><?= $data[name]?></dt>
-        								<dd name="description"><?= $data[description]?></dd>
-        							</dl>
-        						</td>
-        						<td class="link"><a href="detail.php?id=<?= $data[id]?>">詳細</a></td>
-        					</tr>
-    					<?php endforeach;?>
-    				</table>
-			    <?php endif;?>
-			    <?php if(count($records) > 0): ?>
-    				<p><?= count($records) ?>件のレストランが見つかりました。</p>
-    				<table class="list">
-    				    <?php foreach ($records as $record): ?>
-        					<tr>
-        						<td class="photo"><img name="image" alt="「Wine Bar ENOTECA」の写真" src="../pages/img/<?= $record[image]?>" /></td>
-        						<td class="info">
-        							<dl>
-        								<dt name="name"><?= $record[name]?></dt>
-        								<dd name="description"><?= $record[description]?></dd>
-        							</dl>
-        						</td>
-        						<td class="link"><a href="detail.php?id=<?= $record[id]?>">詳細</a></td>
-        					</tr>
-    					<?php endforeach;?>
-    				</table>
-    			<?php endif;?>
+				<p><?= count($records) ?>件のレストランが見つかりました。</p>
+				<table class="list">
+					<?php foreach ($records as $record): ?>
+					<tr>
+						<td class="photo"><img name="image" alt="「<?= $record["name"] ?>」の写真" src="../pages/img/<?= $record["image"] ?>" /></td>
+						<td class="info">
+							<dl>
+								<dt name="name"><?= $record["name"] ?></dt>
+								<dd name="description"><?= $record["description"] ?></dd>
+							</dl>
+						</td>
+						<td class="link"><a href="detail.php?id=<?= $record["id"] ?>">詳細</a></td>
+					</tr>
+					<?php endforeach; ?>
+				</table>
 			</section>
+			<?php endif; ?>
 		</article>
 	</main>
 	<footer>
 		<div class="copyright">&copy; 2020 the applied course of web system development</div>
 	</footer>
 </body>
-</html>
+</php>
